15273851d4f4da42436f9600fded06c9edfc6542
ivan rodriguez <livaiva85@gmail.com>
2024-07-21 12:23:00 +0200

    configuracion de rutas, pagina , vistas y controlador

---
+++ resources/views/index.blade.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Mi Pagina Web</title>
+</head>
+<body>
+    <h1>Bienvenidos a mi pagina web!</h1>
+    <p>Porfavor introduzca sus credenciales</p>
+
+        <form action="ejemplo.php" method="post">
+            <label for="correo">Correo Electrónico:</label><br>
+            <input type="email" id="correo" name="correo" required><br><br>
+    
+            <label for="nombre">Nombre:</label><br>
+            <input type="text" id="nombre" name="nombre" required><br><br>
+
+            <label for="fecha_nacimiento">Fecha de Nacimiento:</label><br>
+            <input type="date" id="fecha_nacimiento" name="fecha_nacimiento" required><br><br>
+
+            <input type="submit" value="Enviar"></body>
+
+
+            </html>
